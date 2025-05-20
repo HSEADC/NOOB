f32e10dccff4fd2b4f8f1648661ec0e4713aba75
Elina Yulun <146091185+elina18yulun@users.noreply.github.com>
2025-05-20 14:45:25 +0300

    commit search

--- src/js/search-vanilla.js
+++ src/js/search-vanilla.js
@@ -10,6 +10,113 @@ document.addEventListener("DOMContentLoaded", () => {
   });
 });
 
+function initSearch() {
+  const O_Search = document.querySelector(".O_Search");
+  const A_SearchInput = O_Search.querySelector(".A_SearchInput");
+  const A_SearchButton = O_Search.querySelector(".A_SearchButton");
+
+  let requestText = getSearchRequest();
+
+  if (requestText != undefined) {
+    A_SearchInput.value = requestText;
+
+    if (content) {
+      SearchContent(requestText);
+    }
+  } else {
+    A_SearchInput.value = "";
+  }
+
+  A_SearchInput.addEventListener("input", (e) => {
+    requestText = e.target.value;
+
+    if (requestText.length >= 3) {
+      A_SearchButton.classList.remove("disable");
+    } else {
+      A_SearchButton.classList.add("disable");
+    }
+  });
+
+  A_SearchInput.addEventListener("keydown", (e) => {
+    requestText = e.target.value;
+
+    if (e.key == "Enter") {
+      setSearchRequest(requestText);
+      SearchContent(requestText);
+    }
+  });
+
+  A_SearchButton.addEventListener("click", (e) => {
+    if (!e.target.classList.contains("disable")) {
+      requestText = A_SearchInput.value;
+
+      setSearchRequest(requestText);
+      SearchContent(requestText);
+    }
+  });
+}
+
+function getSearchRequest() {
+  const url = new URL(window.location.href);
+  const searchParams = new URLSearchParams(url.search);
+
+  if (searchParams.has("request")) {
+    return searchParams.get("request");
+  }
+}
+
+function setSearchRequest(requestText) {
+  const url = window.location.href.split("?")[0];
+
+  window.location.href = url + "?request=" + requestText;
+}
+
+function SearchContent(requestText) {
+  const container = document.querySelector(".S_Content");
+  container.innerHTML = "";
+
+  let contentItemIds = [];
+
+  console.log(content);
+
+  content.forEach((contentItem) => {
+    const nbspRegEx = /[\u202F\u00A0]/gm;
+    const punctuationRegEx = /[.,\/#!$%\^&\*;:{}=\-_`()]/gm;
+
+    let { title, description, id } = contentItem;
+
+    if (requestText.length >= 3) {
+      if (title.includes(requestText) || description.includes(requestText)) {
+        contentItemIds.push(id);
+      } else {
+        console.log("ids");
+      }
+    }
+
+    if (contentItemIds.length > 0) {
+      renderCardsbyIds(container, contentItemIds);
+    } else {
+      renderNothingFounded();
+    }
+  });
+}
+
+function renderNothingFounded() {
+  document.querySelector(".S_Content").innerText = "Ничего не найдено";
+}
+
+function renderCardsbyIds(container, ids) {
+  ids = [...new Set(ids)];
+
+  ids.forEach((id) => {
+    content.forEach((item) => {
+      if (item.id === id) {
+        container.appendChild(createCard(item));
+      }
+    });
+  });
+}
+
 function createCard(contentItemData) {
   const container = document.querySelector(".S_Content");
 
